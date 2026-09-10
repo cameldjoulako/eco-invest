98cb85952f02ae2fc4951f178f807464a4efee24
cameldjoulako <cameldjoulako17@gmail.com>
2026-09-10 23:39:45 +0100

    update test to respect standard

--- src/test/java/com/aroolia/performance/batchfetching/NPlusOneProblemIntegrationTest.java
+++ src/test/java/com/aroolia/performance/batchfetching/NPlusOneProblemIntegrationTest.java
@@ -22,15 +22,23 @@ class NPlusOneProblemIntegrationTest {
     private InvestorService investorService;
 
     @Test
-    @DisplayName("Démonstration du problème N+1 : 1 requête initiale + 10 requêtes unitaires")
-    void givenLazyRelation_whenAccessingPortfolios_thenTriggersNPlusOneQueries() {
+    @DisplayName("Doit déclencher le problème N+1 lors du parcours des collections Lazy")
+    void givenLazyPortfolios_whenGeneratingSummaries_thenTriggersNPlusOne() {
+        // GIVEN: 10 investisseurs ont été insérés en base par DataInitializer
+
+        // WHEN: On récupère les résumés (déclenche 1 requête investisseurs + 10 requêtes portefeuilles)
         log.info(">>>>>> DEBUT DU TEST N+1 <<<<<<");
-
         List<PortfolioSummary> summaries = investorService.getInvestorSummariesWithNPlusOne();
-
         log.info(">>>>>> FIN DU TEST N+1 <<<<<<");
 
-        assertThat(summaries).hasSize(10);
-        assertThat(summaries.get(0).portfolioCount()).isEqualTo(2);
+        // THEN: On vérifie l'intégrité des données retournées
+        assertThat(summaries)
+                .as("La liste des résumés doit contenir les 10 investisseurs")
+                .isNotNull()
+                .hasSize(10);
+
+        assertThat(summaries.get(0).portfolioCount())
+                .as("Chaque investisseur doit posséder 2 portefeuilles")
+                .isEqualTo(2);
     }
 }
